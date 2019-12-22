96c1dfce8ba9567deabd1d3e7d21791ad7820018
loreloc <lorenzoloconte@outlook.it>
2019-12-23 00:52:29 +0100

    added informations of ensemble models fitting

--- ensemble.py
+++ ensemble.py
@@ -6,5 +6,6 @@ class Ensemble:
 
     # Fit each model in the ensemble
     def fit(self, x_train, y_train, epochs=10):
-        for model in self.models:
+        for n, model in enumerate(self.models):
+            print("Fitting model #" + str(n))
             model.fit(x_train, y_train, epochs=epochs, verbose=0)
